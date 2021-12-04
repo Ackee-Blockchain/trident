9b609aaf657de94ea74ae55113f10657fe1c5aa9
Martin Kavík <martin@kavik.cz>
2021-12-04 19:22:17 +0100

    ✨ Standard test.rs

--- crates/trdelnik/src/reader.rs
+++ crates/trdelnik/src/reader.rs
@@ -5,7 +5,7 @@ use anchor_client::{
     }
 };
 use tokio::fs;
-use std::{io, str::FromStr};
+use std::{io, str::FromStr, borrow::Cow};
 use fehler::throws;
 use thiserror::Error;
 use ed25519_dalek::SignatureError;
@@ -22,30 +22,39 @@ pub enum Error {
     KeypairError(#[from] SignatureError),
 }
 
-#[derive(Default)]
-pub struct Reader;
+pub struct Reader {
+    root: Cow<'static, str>
+}
 
 impl Reader {
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            root: "./".into()
+        }
+    }
+
+    pub fn with_root(root: impl Into<Cow<'static, str>>) -> Self {
+        Self {
+            root: root.into()
+        }
     }
 
     #[throws]
     pub async fn pubkey(&self, name: &str) -> Pubkey {
-        let path = format!("./keys/{}_pub.json", name);
+        let path = format!("{}keys/{}_pub.json", self.root, name);
         let key: String = serde_json::from_str(&fs::read_to_string(path).await?)?;
         Pubkey::from_str(&key)?
     }
 
     #[throws]
     pub async fn keypair(&self, name: &str) -> Keypair {
-        let path = format!("./keys/{}.json", name);
+        let path = format!("{}keys/{}.json", self.root, name);
         let bytes: Vec<u8> = serde_json::from_str(&fs::read_to_string(path).await?)?;
         Keypair::from_bytes(&bytes)?
     }
 
     #[throws]
     pub async fn program_data(&self, name: &str) -> Vec<u8> {
-        fs::read(format!("./target/deploy/{}.so", name)).await?
+        fs::read(format!("{}target/deploy/{}.so", self.root, name)).await?
     }
 }
--- examples/state_machine/programs/turnstile/tests/test.rs
+++ examples/state_machine/programs/turnstile/tests/test.rs
@@ -1,4 +1,150 @@
-#[test]
-fn test_turnstile() {
-    assert_eq!(4, 4);
+use anyhow::Error;
+use fehler::throws;
+use trdelnik::*;
+use turnstile::State;
+
+#[throws]
+#[tokio::test]
+async fn test_turnstile() {
+    let mut turnstile = Turnstile {
+        locked: true,
+        reader: Reader::with_root("../../"),
+    };
+    turnstile.locked = turnstile.get_state_client().await?.locked;
+    
+    turnstile.coin().await?;
+    turnstile.push_unlocked().await?;
+    turnstile.push_locked().await?;
 }
+
+struct Turnstile {
+    locked: bool,
+    reader: Reader,
+}
+
+impl Turnstile {
+    #[throws]
+    async fn coin(&mut self) {
+        // inserting a coin is just calling coin
+        self.coin_client().await?;
+
+        // update
+        self.locked = false;
+
+        // get current state
+        let locked_after = self.get_state_client().await?.locked;
+
+        // ensure that coin insert unlocks turnstile
+        assert!(!locked_after);
+    }
+
+    #[throws]
+    async fn push_locked(&mut self) {
+        // get before state
+        let locked_before = self.get_state_client().await?.locked;
+
+        // pushing is just calling push
+        self.push_client().await?;
+
+        // get current state
+        let state = self.get_state_client().await?;
+        let (locked_after, res) = (state.locked, state.res);
+
+        // update
+        self.locked = true;
+
+        // ensure that coin insert unlocks turnstile
+        assert!(!res && locked_after && locked_before);
+    }
+
+    #[throws]
+    async fn push_unlocked(&mut self) {
+        // get before state
+        let locked_before = self.get_state_client().await?.locked;
+
+        // pushing is just calling push
+        self.push_client().await?;
+
+        // get current state
+        let state = self.get_state_client().await?;
+        let (locked_after, res) = (state.locked, state.res);
+
+        // update
+        self.locked = true;
+
+        // ensure that coin insert unlocks turnstile
+        assert!(res && locked_after && !locked_before);
+    }
+
+    #[throws]
+    async fn get_state_client(&self) -> State {
+        let reader = &self.reader;
+        let account_pubkey = reader.pubkey("state").await?;
+        let client = Client::new(reader.keypair("id").await?);
+        client.account_data(account_pubkey).await?
+    }
+
+    #[throws]
+    async fn coin_client(&self) {
+        let reader = &self.reader;
+        let payer = reader.keypair("id").await?;
+        Client::new(payer).send_instruction(
+            reader.pubkey("program").await?,
+            turnstile::instruction::Coin,
+            turnstile::accounts::UpdateState { 
+                state: reader.pubkey("state").await?
+            },
+            None,
+        ).await?;
+    }
+
+    #[throws]
+    async fn push_client(&self) {
+        let reader = &self.reader;
+        let payer = reader.keypair("id").await?;
+        Client::new(payer).send_instruction(
+            reader.pubkey("program").await?,
+            turnstile::instruction::Push,
+            turnstile::accounts::UpdateState { 
+                state: reader.pubkey("state").await?
+            },
+            None,
+        ).await?;
+    }
+
+    #[throws]
+    async fn _init_client(&self) {
+        let reader = &self.reader;
+
+        let payer = reader.keypair("id").await?;
+        let payer_pubkey = payer.pubkey();
+
+        let program_keypair = reader.keypair("program").await?;
+        let program_pubkey = program_keypair.pubkey();
+        let program_data = reader.program_data("turnstile").await?;
+
+        let client = Client::new(payer);
+
+        println!("AIRDROP");
+        client.airdrop(payer_pubkey, 5_000_000_000).await?;
+
+        println!("DEPLOY");
+        client.deploy(program_keypair, program_data).await?;
+
+        println!("INIT STATE");
+        let state = reader.keypair("state").await?;
+        client.send_instruction(
+            program_pubkey,
+            turnstile::instruction::Initialize,
+            turnstile::accounts::Initialize { 
+                state: state.pubkey(),
+                user: payer_pubkey,
+                system_program: System::id()
+            },
+            Some(state),
+        ).await?;
+
+        println!("Initialized");
+    }
+}
+
