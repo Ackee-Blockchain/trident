35be7c56dcdba12755e30d82c6e2276542d0bc8b
Martin Kavík <martin@kavik.cz>
2022-02-09 16:43:48 +0100

    ✨ Automatically create program_client crate

--- crates/trdelnik_cli/src/command/test.rs
+++ crates/trdelnik_cli/src/command/test.rs
@@ -5,7 +5,9 @@ use trdelnik::*;
 #[throws]
 pub async fn test(root: String) {
     let commander = Commander::with_root(root);
+    commander.create_program_client_crate().await?;
     commander.build_programs().await?;
+    commander.generate_program_client_deps().await?;
     commander.generate_program_client_lib_rs().await?;
     commander.run_tests().await?;
     println!("Trdelnik CLI: Test command finished");
