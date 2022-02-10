5dc8e645e57684fbbb9bcb068e29da58451a414a
Martin Kavík <martin@kavik.cz>
2022-02-10 14:25:20 +0100

    :white_check_mark: Add trdelnik_test macro tests

---
+++ crates/trdelnik_test/tests/expand/with_root.rs
@@ -0,0 +1,4 @@
+#[trdelnik_test::trdelnik_test(root = "i_am_root")]
+async fn test_with_defined_root() {
+    println!("Hello!");
+}
---
+++ crates/trdelnik_test/tests/test.rs
@@ -0,0 +1,4 @@
+#[test]
+pub fn expanded_trdelnik_test() {
+    macrotest::expand("tests/expand/*.rs");
+}
