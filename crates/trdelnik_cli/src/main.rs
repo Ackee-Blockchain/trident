edd8850141c1e711fb389eeb44a855f2dee24f66
Lukas Kozak <97888562+hotshotcoder@users.noreply.github.com>
2022-02-17 19:13:13 +0100

    🎨 Refactor trdelnik_cli package

--- crates/trdelnik_cli/src/main.rs
+++ crates/trdelnik_cli/src/main.rs
@@ -1,24 +1,8 @@
 use anyhow::Error;
-use clap::Parser;
 use fehler::throws;
-
-mod command;
-
-/// Trdelnik CLI
-#[derive(Parser, Debug)]
-enum Args {
-    /// Run program tests
-    Test {
-        /// Anchor project root
-        #[clap(short, long, default_value = "./")]
-        root: String,
-    },
-}
 
 #[throws]
 #[tokio::main]
 async fn main() {
-    match Args::parse() {
-        Args::Test { root } => command::test(root).await?,
-    }
+    trdelnik_cli::start().await?
 }
