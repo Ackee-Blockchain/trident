2eaffa7e58e2851e292dbe9a76de643262ed482c
Vladimír Marcin <marcinvlado@gmail.com>
2022-03-03 11:18:55 +0100

    ✨ Split test command into build + test
    build command builds the program_client crate and test command just run tests afterwards

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -13,6 +13,12 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
+    /// Create a `program_client` crate 
+    Build {
+        /// Anchor project root
+        #[clap(short, long, default_value = "./")]
+        root: String,
+    },
     /// Run program tests
     Test {
         /// Anchor project root
@@ -28,6 +34,7 @@ pub async fn start() {
     let cli = Cli::parse();
 
     match cli.command {
+        Command::Build { root } => command::build(root).await?,
         Command::Test { root } => command::test(root).await?,
         Command::Localnet => command::localnet().await?,
     }
