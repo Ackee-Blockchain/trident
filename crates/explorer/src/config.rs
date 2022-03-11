80b55a487671d214637f48478d408fc7896d18d6
Lukas Kozak <97888562+hotshotcoder@users.noreply.github.com>
2022-03-11 20:48:03 +0100

    ✨ feat(explorer): add config module

--- crates/explorer/src/config.rs
+++ crates/explorer/src/config.rs
@@ -1,34 +1,70 @@
-use solana_cli_config::Config;
+use solana_cli_config::{Config, CONFIG_FILE};
 use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::commitment_config::CommitmentConfig;
 
-struct ExplorerConfig {
-    solana_config: Config,
+pub struct ExplorerConfig {
+    json_rpc_url: String,
     rpc_client: RpcClient,
     verbose: bool,
     colored: bool,
 }
 
-// impl Config {
-//     fn new() {
-//         let config = {
-//             let cli_config = if let Some(config_file) = matches.value_of("config_file") {
-//                 solana_cli_config::Config::load(config_file).unwrap_or_default()
-//             } else {
-//                 solana_cli_config::Config::default()
-//             };
+impl ExplorerConfig {
+    pub fn new() -> Self {
+        let json_rpc_url = if let Some(config_file) = &*CONFIG_FILE {
+            Config::load(config_file).unwrap_or_default().json_rpc_url
+        } else {
+            Config::default().json_rpc_url
+        };
 
-//             Config {
-//                 json_rpc_url: matches
-//                     .value_of("json_rpc_url")
-//                     .unwrap_or(&cli_config.json_rpc_url)
-//                     .to_string(),
-//                 keypair: read_keypair_file(
-//                     matches
-//                         .value_of("keypair")
-//                         .unwrap_or(&cli_config.keypair_path),
-//                 )?,
-//                 verbose: matches.is_present("verbose"),
-//             }
-//         };
-//     }
-// }
+        let rpc_client =
+            RpcClient::new_with_commitment(json_rpc_url.clone(), CommitmentConfig::confirmed());
+
+        solana_logger::setup_with("solana=debug");
+
+        ExplorerConfig {
+            json_rpc_url,
+            rpc_client,
+            verbose: true,
+            colored: true,
+        }
+    }
+
+    pub fn json_rpc_url(&self) -> &String {
+        &self.json_rpc_url
+    }
+
+    pub fn set_json_rpc_url(&mut self, new_json_rpc_url: String) {
+        self.json_rpc_url = new_json_rpc_url;
+        self.rpc_client = RpcClient::new_with_commitment(
+            self.json_rpc_url.clone(),
+            CommitmentConfig::confirmed(),
+        );
+    }
+
+    pub fn rpc_client(&self) -> &RpcClient {
+        &self.rpc_client
+    }
+
+    pub fn verbose(&self) -> bool {
+        self.verbose
+    }
+
+    pub fn set_verbose(&mut self, new_verbose: bool) {
+        self.verbose = new_verbose;
+    }
+
+    pub fn colored(&self) -> bool {
+        self.colored
+    }
+
+    pub fn set_colored(&mut self, new_colored: bool) {
+        self.colored = new_colored;
+    }
+}
+
+impl Default for ExplorerConfig {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- crates/explorer/src/lib.rs
+++ crates/explorer/src/lib.rs
@@ -1,1 +1,3 @@
-mod config;
+pub mod config;
+
+pub mod error;
