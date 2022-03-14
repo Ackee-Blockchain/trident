1e0ac1f7b8ebbf7a359e7ef5c591fe0e2210a8e8
Lukas Kozak <97888562+hotshotcoder@users.noreply.github.com>
2022-03-14 01:25:18 +0100

    ✨ feat(explorer): add output module

--- crates/cli/src/command/explorer/account.rs
+++ crates/cli/src/command/explorer/account.rs
@@ -1,10 +1,17 @@
 use anyhow::Error;
 use fehler::throws;
 use solana_sdk::pubkey::Pubkey;
-use trdelnik_explorer::account::AccountQueryBuilder;
+use trdelnik_explorer::{
+    account::{AccountFieldVisibility, AccountQueryBuilder},
+    display::DisplayFormat,
+    output::get_account_string,
+};
 
 #[throws]
 pub async fn view(pubkey: Pubkey) {
     let query = AccountQueryBuilder::with_pubkey(pubkey).build();
     let account = query.fetch_one().await?;
+    let visibility = AccountFieldVisibility::new_all_enabled();
+    let result = get_account_string(&account, &visibility, DisplayFormat::Trdelnik)?;
+    print!("{}", result);
 }
