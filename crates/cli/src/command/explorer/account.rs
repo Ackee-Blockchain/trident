867d278ac433fa956a48ba064d5a47f93146de31
Lukas Kozak <97888562+hotshotcoder@users.noreply.github.com>
2022-03-15 20:21:31 +0100

    🎨 feat(explorer): beautify account output and refactor

--- crates/cli/src/command/explorer/account.rs
+++ crates/cli/src/command/explorer/account.rs
@@ -3,7 +3,7 @@ use fehler::throws;
 use solana_sdk::pubkey::Pubkey;
 use trdelnik_explorer::{
     account::{AccountFieldVisibility, AccountQueryBuilder},
-    display::DisplayAccountFormat,
+    display::DisplayFormat,
     output::get_account_string,
 };
 
@@ -12,6 +12,6 @@ pub async fn view(pubkey: Pubkey) {
     let query = AccountQueryBuilder::with_pubkey(pubkey).build();
     let account = query.fetch_one().await?;
     let visibility = AccountFieldVisibility::new_all_enabled();
-    let result = get_account_string(&account, &visibility, DisplayAccountFormat::Trdelnik)?;
-    print!("{}", result);
+    let result = get_account_string(&account, &visibility, DisplayFormat::Trdelnik)?;
+    println!("{}", result);
 }
