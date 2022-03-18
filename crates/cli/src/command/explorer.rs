7419b76f149fc2100048f712f1e23008fc697e73
Lukas Kozak <97888562+hotshotcoder@users.noreply.github.com>
2022-03-18 02:04:03 +0100

    ✨ feat(cli): implement account and program queries

--- crates/cli/src/command/explorer.rs
+++ crates/cli/src/command/explorer.rs
@@ -2,6 +2,7 @@ use anyhow::Error;
 use clap::Subcommand;
 use fehler::throws;
 use solana_sdk::{pubkey::Pubkey, signature::Signature};
+use trdelnik_explorer::display::{AccountDisplayFormat, ProgramDisplayFormat};
 
 mod account;
 mod program;
@@ -13,10 +14,18 @@ pub enum ExplorerCommand {
     Account {
         /// Ed25519 pubkey, PDA or hash of a pubkey
         pubkey: Pubkey,
+        #[clap(long = "json-pretty")]
+        jsonpretty: bool,
+        #[clap(long, conflicts_with = "jsonpretty")]
+        json: bool,
     },
     Program {
         /// Address of a program to show
         pubkey: Pubkey,
+        #[clap(long = "json-pretty")]
+        jsonpretty: bool,
+        #[clap(long, conflicts_with = "jsonpretty")]
+        json: bool,
     },
     /// Show the contents of a transaction
     Transaction {
@@ -28,8 +37,32 @@ pub enum ExplorerCommand {
 #[throws]
 pub async fn explorer(subcmd: ExplorerCommand) {
     match subcmd {
-        ExplorerCommand::Account { pubkey } => account::view(pubkey).await?,
-        ExplorerCommand::Program { pubkey } => program::view(pubkey).await?,
+        ExplorerCommand::Account {
+            pubkey,
+            jsonpretty,
+            json,
+        } => {
+            if jsonpretty {
+                account::view(pubkey, AccountDisplayFormat::JSONPretty).await?
+            } else if json {
+                account::view(pubkey, AccountDisplayFormat::JSON).await?
+            } else {
+                account::view(pubkey, AccountDisplayFormat::Trdelnik).await?
+            }
+        }
+        ExplorerCommand::Program {
+            pubkey,
+            jsonpretty,
+            json,
+        } => {
+            if jsonpretty {
+                program::view(pubkey, ProgramDisplayFormat::JSONPretty).await?
+            } else if json {
+                program::view(pubkey, ProgramDisplayFormat::JSON).await?
+            } else {
+                program::view(pubkey, ProgramDisplayFormat::Trdelnik).await?
+            }
+        }
         ExplorerCommand::Transaction { signature } => transaction::view(signature).await?,
     }
 }
--- crates/cli/src/command/explorer/account.rs
+++ crates/cli/src/command/explorer/account.rs
@@ -3,15 +3,15 @@ use fehler::throws;
 use solana_sdk::pubkey::Pubkey;
 use trdelnik_explorer::{
     account::{AccountFieldVisibility, AccountQueryBuilder},
-    display::DisplayFormat,
+    display::AccountDisplayFormat,
     output::get_account_string,
 };
 
 #[throws]
-pub async fn view(pubkey: Pubkey) {
+pub async fn view(pubkey: Pubkey, format: AccountDisplayFormat) {
     let query = AccountQueryBuilder::with_pubkey(pubkey).build();
     let account = query.fetch_one().await?;
     let visibility = AccountFieldVisibility::new_all_enabled();
-    let result = get_account_string(&account, &visibility, DisplayFormat::Trdelnik)?;
-    print!("{}", result);
+    let result = get_account_string(&account, &visibility, format)?;
+    println!("{}", result);
 }
