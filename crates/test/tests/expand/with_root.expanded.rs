2ac3a61f009f723ffe42444617bfcb9da901c9b5
Tibor Tribus <tribuste@gmail.com>
2022-04-28 11:59:52 +0200

    🐛 Fix trdelnik macro expansion, Uncomment crates

--- crates/test/tests/expand/with_root.expanded.rs
+++ crates/test/tests/expand/with_root.expanded.rs
@@ -4,11 +4,7 @@ async fn test_with_defined_root() -> trdelnik_client::anyhow::Result<()> {
     let mut tester = trdelnik_client::Tester::with_root("i_am_root");
     let localnet_handle = tester.before().await?;
     let test = async {
-        {
-            {
-                ::std::io::_print(::core::fmt::Arguments::new_v1(&["Hello!\n"], &[]));
-            };
-        }
+        {}
         Ok::<(), trdelnik_client::anyhow::Error>(())
     };
     let result = std::panic::AssertUnwindSafe(test).catch_unwind().await;
--- crates/test/tests/expand/with_root.rs
+++ crates/test/tests/expand/with_root.rs
@@ -1,4 +1,3 @@
 #[trdelnik_test::trdelnik_test(root = "i_am_root")]
 async fn test_with_defined_root() {
-    println!("Hello!");
 }
