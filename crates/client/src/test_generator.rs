10b2f160ec22eb91583bf06b51bb5c70fcd5aa27
Stefan Prokop <sprokop1010@seznam.cz>
2022-05-02 14:29:19 +0200

    ✅ add trdelnik tests

--- crates/client/src/test_generator.rs
+++ crates/client/src/test_generator.rs
@@ -1,24 +1,18 @@
-use fehler::throws;
+use fehler::{throw, throws};
 use std::env::current_dir;
-use std::fmt::format;
 use std::path::{Path, PathBuf};
-use serde::Serialize;
 use thiserror::Error;
 use tokio::fs;
-use toml::{value::Table, Value, ser::Serializer};
 
 const TESTS_DIRECTORY: &str = "tests";
+const TESTS_FILE_NAME: &str = "test.rs";
+const CARGO_TOML: &str = "Cargo.toml";
+const ANCHOR_TOML: &str = "Anchor.toml";
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("invalid workspace")]
     BadWorkspace,
-    #[error("cannot initialize dev dependencies")]
-    CannotInitDevDeps,
-    #[error("cannot parse Cargo.toml")]
-    CannotParseCargoToml,
-    #[error("{0:?}")]
-    Toml(#[from] toml::de::Error),
     #[error("{0:?}")]
     Io(#[from] std::io::Error),
 }
@@ -38,7 +32,6 @@ impl TestGenerator {
     pub async fn generate(&self) {
         self.check_workspace().await?;
         self.generate_test_files().await?;
-        self.add_dev_dependencies().await?;
     }
 
     #[throws]
@@ -47,46 +40,35 @@ impl TestGenerator {
         if fs::metadata(&path).await.is_ok() {
             return;
         }
-
         fs::create_dir(&path).await?;
-        fs::write(path.join("test.rs"), "").await?;
+        fs::write(path.join(TESTS_FILE_NAME), "").await?;
+        self.initialize_cargo_toml().await?;
     }
 
     #[throws]
-    async fn add_dev_dependencies(&self) {
-        let cargo_toml = Path::new(&self.path).join("Cargo.toml");
-        let mut cargo_toml_content: Value = fs::read_to_string(&cargo_toml).await?.parse()?;
-        let mut dev_deps = cargo_toml_content
-            .as_table_mut()
-            .ok_or(Error::CannotParseCargoToml)?
-            .entry("dev-dependencies")
-            .or_insert(Value::Table(Table::default()))
-            .as_table_mut()
-            .ok_or(Error::CannotInitDevDeps)?;
-        dev_deps = self.initialize_dev_deps(dev_deps);
-        println!("{:?}\n\n", dev_deps);
-        println!("{:?}", cargo_toml_content.to_string());
-        // fs::write(cargo_toml, cargo_toml_content.to_string()).await?;
-    }
+    async fn initialize_cargo_toml(&self) {
+        let cargo_toml = Path::new(&self.path).join(TESTS_DIRECTORY).join(CARGO_TOML);
+        // todo: the `trdelnik-client` path should be changed to crate version after the release
+        let toml = r#"[package]
+name = "tests"
+version = "0.1.0"
+description = "Created with Trdelnik"
+edition = "2021"
 
-    fn initialize_dev_deps<'a>(&'a self, dev_deps: &'a mut Table) -> &mut Table {
-        [
-            r#"fehler = "1.0.0""#,
-            r#"trdelnik-client = { path = "../../../../crates/client" }"#,
-            r#"program_client = { path = "../../program_client" }"#,
-        ]
-            .map(|dependency| {
-                if let Value::Table(table) = dependency.parse().unwrap() {
-                    let (name, value) = table.into_iter().next().unwrap();
-                    dev_deps.entry(name).or_insert(value.clone());
-                }
-            });
-        dev_deps
+[dev-dependencies]
+fehler = "1.0.0"
+trdelnik-client = { path = "../../../crates/client" }
+program_client = { path = "../program_client" }
+"#;
+        fs::write(cargo_toml, toml).await?;
     }
 
     #[throws]
     async fn check_workspace(&self) {
-        // todo: throw error if the workspace is not valid
-        // throw!(Error::BadWorkspace);
+        let anchor_toml = Path::new(&self.path).join(ANCHOR_TOML);
+        match anchor_toml.exists() {
+            false => throw!(Error::BadWorkspace),
+            _ => {}
+        }
     }
 }
