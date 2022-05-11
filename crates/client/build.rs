723ad277060fab718745a5883da4d921ef8145a9
Tibor Tribus <tribuste@gmail.com>
2022-05-11 15:23:23 +0200

    🔧 Update all crates Cargo.toml for release

--- crates/client/build.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-use vergen::{vergen, Config};
-
-fn main() -> Result<()> {
-    // `vergen` creates env variables with info about
-    // the current commit hash or the crate version.
-    vergen(Config::default())
-}
