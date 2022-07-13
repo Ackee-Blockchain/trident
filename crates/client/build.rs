565932c811a7173c46048c600ff50625bf600dab
Stefan Prokop <sprokop1010@seznam.cz>
2022-07-13 13:22:02 +0200

    🐛 Fix vergen build & upgrade deps

--- crates/client/build.rs
+++ crates/client/build.rs
@@ -1,8 +1,15 @@
 use anyhow::Result;
-use vergen::{vergen, Config};
+// todo: use vergen before you want to uncomment comments in `generate_program_client_deps`
+//  (commander.rs lines 198 - 207)
+//  - this code is commented because of a problem with vergen's `git` feature during publishing the crate
+//  - the problem was: it wasn't able to find the repository (commit hashes) because the published crate
+//      does not contain the .git folder (make sure this is fixed before the publish)
+//  - before you uncomment the mentioned code, don't forget to add the `git` feature to Cargo.toml
+// use vergen::{vergen, Config};
 
 fn main() -> Result<()> {
     // `vergen` creates env variables with info about
     // the current commit hash or the crate version.
-    vergen(Config::default())
+    // vergen(Config::default())
+    Ok(())
 }
