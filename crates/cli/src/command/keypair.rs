40c77a42f620fa5e30abc1d07e8fc3ba0e04c3c4
Antonín Kříž <15214494+antoninkriz@users.noreply.github.com>
2022-07-13 13:55:20 +0200

    ✨ Added the key-pair command to print information about a key-pair

---
+++ crates/cli/src/command/keypair.rs
@@ -0,0 +1,24 @@
+use anyhow::Error;
+use clap::Subcommand;
+use fehler::throws;
+use solana_sdk::signer::Signer;
+use trdelnik_client::{keypair as other_keypair, program_keypair, system_keypair};
+
+#[derive(Subcommand)]
+pub enum KeyPairCommand {
+    Program { n: usize },
+    System { n: usize },
+    Other { n: usize },
+}
+
+#[throws]
+pub fn keypair(subcmd: KeyPairCommand) {
+    let kp: solana_sdk::signer::keypair::Keypair = match subcmd {
+        KeyPairCommand::Program { n } => program_keypair(n),
+        KeyPairCommand::System { n } => system_keypair(n),
+        KeyPairCommand::Other { n } => other_keypair(n),
+    };
+
+    println!("PubKey: {:?}", kp.pubkey());
+    println!("KeyPair: {:?}", kp.to_bytes());
+}
