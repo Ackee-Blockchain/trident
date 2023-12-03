b10185291872fa28129a26d99cf6cc61c9b3f1ad
lukacan <andrej@DESKTOP-UME2160.localdomain>
2023-12-03 21:18:52 +0100

    🎨 added discover into clean, and moved clean command

--- crates/cli/src/command/clean.rs
+++ crates/cli/src/command/clean.rs
@@ -1,9 +1,17 @@
-use anyhow::Error;
+use anyhow::{bail, Error};
 use fehler::throws;
-use trdelnik_client::Cleaner;
+use trdelnik_client::WorkspaceBuilder;
+
+use crate::discover;
+pub const TRDELNIK_TOML: &str = "Trdelnik.toml";
 
 #[throws]
 pub async fn clean() {
-    let cleaner = Cleaner::new();
-    cleaner.clean_target().await?;
+    let root = if let Some(r) = discover(TRDELNIK_TOML)? {
+        r
+    } else {
+        bail!("It does not seem that Trdelnik is initialized because the Trdelnik.toml file was not found in any parent directory!");
+    };
+    let builder = WorkspaceBuilder::new_with_root(root);
+    builder.clean().await?;
 }
