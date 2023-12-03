2d07c264f6b0f4780a672cc0f2d973320f517b7f
lukacan <andrej@DESKTOP-UME2160.localdomain>
2023-12-03 21:18:20 +0100

    🎨 added discover into build, added arch into build

--- crates/cli/src/command/build.rs
+++ crates/cli/src/command/build.rs
@@ -1,12 +1,19 @@
-use anyhow::Error;
+use crate::{discover, ProgramArch};
+use anyhow::{bail, Error};
 use fehler::throws;
 use trdelnik_client::*;
 
+pub const TRDELNIK_TOML: &str = "Trdelnik.toml";
+
 #[throws]
-pub async fn build(root: String) {
-    let commander = Commander::with_root(root);
-    commander.create_program_client_crate().await?;
-    commander.build_programs().await?;
-    commander.generate_program_client_deps().await?;
-    commander.generate_program_client_lib_rs().await?;
+pub async fn build(root: String, arch: ProgramArch) {
+    // TODO root argument maybe not needed
+    let root = if let Some(r) = discover(TRDELNIK_TOML)? {
+        r
+    } else {
+        bail!("It does not seem that Trdelnik is initialized because the Trdelnik.toml file was not found in any parent directory!");
+    };
+    let mut builder = WorkspaceBuilder::new_with_root(root);
+    let arch = arch.build_subcommand();
+    builder.build(arch).await?;
 }
