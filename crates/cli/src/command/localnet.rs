569861f4fcfa3b652e1634a43f836854b8b6fe9c
lukacan <andrej@DESKTOP-UME2160.localdomain>
2023-12-03 21:21:14 +0100

    🎨 added discover into localnet

--- crates/cli/src/command/localnet.rs
+++ crates/cli/src/command/localnet.rs
@@ -1,12 +1,20 @@
-use anyhow::Error;
+use anyhow::{bail, Error};
 use fehler::throws;
 use tokio::signal;
 use trdelnik_client::*;
 
+use crate::discover;
+pub const TRDELNIK_TOML: &str = "Trdelnik.toml";
 #[throws]
 pub async fn localnet() {
-    let commander = Commander::new();
-    let validator_handle = commander.start_localnet().await?;
+    // TODO this is only starting local validator , so maybe no need to check for Trdelnik.toml
+    let root = if let Some(r) = discover(TRDELNIK_TOML)? {
+        r
+    } else {
+        bail!("It does not seem that Trdelnik is initialized because the {} file was not found in any parent directory!",TRDELNIK_TOML);
+    };
+
+    let validator_handle = Commander::start_localnet(&root).await?;
 
     // wait for SIGINT (^C) signal
     signal::ctrl_c().await.expect("failed to listen for event");
