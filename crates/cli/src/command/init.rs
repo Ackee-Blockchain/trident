bd3ed402c3b83c38c29c4cef00312bfe2501b826
lukacan <andrej@DESKTOP-UME2160.localdomain>
2023-12-03 21:20:41 +0100

    🎨 also added functionality for sbf/bpf arch and also correctly check for anchor/cargo toml files for fuzzer/no fuzzer init

--- crates/cli/src/command/init.rs
+++ crates/cli/src/command/init.rs
@@ -1,9 +1,34 @@
-use anyhow::Error;
+use anyhow::{bail, Error};
 use fehler::throws;
-use trdelnik_client::TestGenerator;
+use trdelnik_client::WorkspaceBuilder;
+
+use crate::{discover, ProgramArch};
+
+pub const ANCHOR_TOML: &str = "Anchor.toml";
+pub const CARGO_TOML: &str = "Cargo.toml";
 
 #[throws]
-pub async fn init(skip_fuzzer: bool) {
-    let generator = TestGenerator::new();
-    generator.generate(skip_fuzzer).await?;
+pub async fn init(skip_fuzzer: bool, arch: ProgramArch) {
+    // TODO maybe remove skip fuzzer option ?
+    if skip_fuzzer {
+        // if skipping fuzzer no need to look for anchor.toml
+        let root = if let Some(r) = discover(CARGO_TOML)? {
+            r
+        } else {
+            bail!("It does not seem that project is initialized because the Cargo.toml file was not found in any parent directory!");
+        };
+        let mut generator = WorkspaceBuilder::new_with_root(root);
+        let arch = arch.build_subcommand();
+        generator.initialize_without_fuzzer(arch).await?;
+    } else {
+        // fuzzer only supported with anchor
+        let root = if let Some(r) = discover(ANCHOR_TOML)? {
+            r
+        } else {
+            bail!("It does not seem that Anchor is initialized because the Anchor.toml file was not found in any parent directory!");
+        };
+        let mut generator = WorkspaceBuilder::new_with_root(root);
+        let arch = arch.build_subcommand();
+        generator.initialize_with_fuzzer(arch).await?;
+    }
 }
