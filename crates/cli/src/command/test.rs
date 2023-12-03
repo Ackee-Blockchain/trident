13988789d29c4e46d047cb258ddf03aca98eac31
lukacan <andrej@DESKTOP-UME2160.localdomain>
2023-12-03 21:21:34 +0100

    🎨 discover added for test command

--- crates/cli/src/command/test.rs
+++ crates/cli/src/command/test.rs
@@ -1,9 +1,19 @@
-use anyhow::Error;
+use anyhow::{bail, Error};
 use fehler::throws;
 use trdelnik_client::*;
 
+use crate::discover;
+
+pub const TRDELNIK_TOML: &str = "Trdelnik.toml";
+
 #[throws]
 pub async fn test(root: String) {
-    let commander = Commander::with_root(root);
-    commander.run_tests().await?;
+    // TODO root argument maybe not needed
+
+    match discover(TRDELNIK_TOML)? {
+        Some(_) => Commander::run_tests().await?,
+        _ => {
+            bail!("It does not seem that Trdelnik is initialized because the Trdelnik.toml file was not found in any parent directory!")
+        }
+    }
 }
