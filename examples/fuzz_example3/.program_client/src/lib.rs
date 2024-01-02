5d9e98e07955453e5dbe4058ab485bea298e50ff
lukacan <andrej@DESKTOP-UME2160.localdomain>
2024-01-02 15:33:11 +0100

    ✅ added fuzz example3 - escrow with bad math

---
+++ examples/fuzz_example3/.program_client/src/lib.rs
@@ -0,0 +1,140 @@
+// DO NOT EDIT - automatically generated file (except `use` statements inside the `*_instruction` module
+pub mod fuzz_example3_instruction {
+    use trdelnik_client::*;
+    pub static PROGRAM_ID: Pubkey = Pubkey::new_from_array([
+        222u8, 219u8, 96u8, 222u8, 150u8, 129u8, 32u8, 71u8, 184u8, 221u8, 54u8, 221u8, 224u8,
+        97u8, 103u8, 133u8, 11u8, 126u8, 234u8, 11u8, 186u8, 25u8, 119u8, 161u8, 48u8, 137u8, 77u8,
+        249u8, 144u8, 153u8, 133u8, 92u8,
+    ]);
+    pub async fn init_vesting(
+        client: &Client,
+        i_recipient: Pubkey,
+        i_amount: u64,
+        i_start_at: u64,
+        i_end_at: u64,
+        i_interval: u64,
+        a_sender: Pubkey,
+        a_sender_token_account: Pubkey,
+        a_escrow: Pubkey,
+        a_escrow_token_account: Pubkey,
+        a_mint: Pubkey,
+        a_token_program: Pubkey,
+        a_system_program: Pubkey,
+        signers: impl IntoIterator<Item = Keypair> + Send + 'static,
+    ) -> Result<EncodedConfirmedTransactionWithStatusMeta, ClientError> {
+        client
+            .send_instruction(
+                PROGRAM_ID,
+                fuzz_example3::instruction::InitVesting {
+                    recipient: i_recipient,
+                    amount: i_amount,
+                    start_at: i_start_at,
+                    end_at: i_end_at,
+                    interval: i_interval,
+                },
+                fuzz_example3::accounts::InitVesting {
+                    sender: a_sender,
+                    sender_token_account: a_sender_token_account,
+                    escrow: a_escrow,
+                    escrow_token_account: a_escrow_token_account,
+                    mint: a_mint,
+                    token_program: a_token_program,
+                    system_program: a_system_program,
+                },
+                signers,
+            )
+            .await
+    }
+    pub fn init_vesting_ix(
+        i_recipient: Pubkey,
+        i_amount: u64,
+        i_start_at: u64,
+        i_end_at: u64,
+        i_interval: u64,
+        a_sender: Pubkey,
+        a_sender_token_account: Pubkey,
+        a_escrow: Pubkey,
+        a_escrow_token_account: Pubkey,
+        a_mint: Pubkey,
+        a_token_program: Pubkey,
+        a_system_program: Pubkey,
+    ) -> Instruction {
+        Instruction {
+            program_id: PROGRAM_ID,
+            data: fuzz_example3::instruction::InitVesting {
+                recipient: i_recipient,
+                amount: i_amount,
+                start_at: i_start_at,
+                end_at: i_end_at,
+                interval: i_interval,
+            }
+            .data(),
+            accounts: fuzz_example3::accounts::InitVesting {
+                sender: a_sender,
+                sender_token_account: a_sender_token_account,
+                escrow: a_escrow,
+                escrow_token_account: a_escrow_token_account,
+                mint: a_mint,
+                token_program: a_token_program,
+                system_program: a_system_program,
+            }
+            .to_account_metas(None),
+        }
+    }
+    pub async fn withdraw_unlocked(
+        client: &Client,
+        a_recipient: Pubkey,
+        a_recipient_token_account: Pubkey,
+        a_escrow: Pubkey,
+        a_escrow_token_account: Pubkey,
+        a_escrow_pda_authority: Pubkey,
+        a_mint: Pubkey,
+        a_token_program: Pubkey,
+        a_system_program: Pubkey,
+        signers: impl IntoIterator<Item = Keypair> + Send + 'static,
+    ) -> Result<EncodedConfirmedTransactionWithStatusMeta, ClientError> {
+        client
+            .send_instruction(
+                PROGRAM_ID,
+                fuzz_example3::instruction::WithdrawUnlocked {},
+                fuzz_example3::accounts::WithdrawUnlocked {
+                    recipient: a_recipient,
+                    recipient_token_account: a_recipient_token_account,
+                    escrow: a_escrow,
+                    escrow_token_account: a_escrow_token_account,
+                    escrow_pda_authority: a_escrow_pda_authority,
+                    mint: a_mint,
+                    token_program: a_token_program,
+                    system_program: a_system_program,
+                },
+                signers,
+            )
+            .await
+    }
+    pub fn withdraw_unlocked_ix(
+        a_recipient: Pubkey,
+        a_recipient_token_account: Pubkey,
+        a_escrow: Pubkey,
+        a_escrow_token_account: Pubkey,
+        a_escrow_pda_authority: Pubkey,
+        a_mint: Pubkey,
+        a_token_program: Pubkey,
+        a_system_program: Pubkey,
+    ) -> Instruction {
+        Instruction {
+            program_id: PROGRAM_ID,
+            data: fuzz_example3::instruction::WithdrawUnlocked {}.data(),
+            accounts: fuzz_example3::accounts::WithdrawUnlocked {
+                recipient: a_recipient,
+                recipient_token_account: a_recipient_token_account,
+                escrow: a_escrow,
+                escrow_token_account: a_escrow_token_account,
+                escrow_pda_authority: a_escrow_pda_authority,
+                mint: a_mint,
+                token_program: a_token_program,
+                system_program: a_system_program,
+            }
+            .to_account_metas(None),
+        }
+    }
+}
---
+++ examples/fuzz_example3/programs/fuzz_example3/src/state.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct Escrow {
+    pub recipient: Pubkey,
+    pub amount: u64,
+    pub withdrawal: u64,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub interval: u64,
+    pub bump: u8,
+}
+
+impl Escrow {
+    pub fn amount_unlocked(&self, now: u64) -> Option<u64> {
+        // take minimum from now and end time
+        let time = if now < self.end_time {
+            now
+        } else {
+            self.end_time
+        };
+
+        // end_at - start_at, difference of timestamps in second
+        let duration = self.end_time.checked_sub(self.start_time)?;
+
+        // amount * interval / duration
+        let interval_amount = self
+            .amount
+            .checked_mul(self.interval)?
+            .checked_div(duration)?;
+        msg!("interval_amount {}", interval_amount);
+
+        // (time - self.start_at) / self.interval + 1, current and passed intervals are unlocked
+        let nr_intervals = time
+            .checked_sub(self.start_time)?
+            .checked_div(self.interval)?
+            .checked_add(1)?;
+        msg!("nr_intervals {}", nr_intervals);
+
+        // nr_intervals * interval_amount - self.withdrawal
+        nr_intervals
+            .checked_mul(interval_amount)?
+            .checked_sub(self.withdrawal)
+    }
+}
