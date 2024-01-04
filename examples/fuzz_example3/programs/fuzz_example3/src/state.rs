397019fad7c1208a8db594e2159953d7a779ffe4
lukacan <andrej@DESKTOP-UME2160.localdomain>
2024-01-04 21:52:43 +0100

    ✅ minor program example updates

--- examples/fuzz_example3/programs/fuzz_example3/src/state.rs
+++ examples/fuzz_example3/programs/fuzz_example3/src/state.rs
@@ -13,33 +13,29 @@ pub struct Escrow {
 
 impl Escrow {
     pub fn amount_unlocked(&self, now: u64) -> Option<u64> {
-        // take minimum from now and end time
         let time = if now < self.end_time {
             now
         } else {
             self.end_time
         };
 
-        // end_at - start_at, difference of timestamps in second
         let duration = self.end_time.checked_sub(self.start_time)?;
         msg!("duration {}", duration);
         msg!("amount {}", self.amount);
         msg!("interval {}", self.interval);
-        // amount * interval / duration
+
         let interval_amount = self
             .amount
             .checked_mul(self.interval)?
             .checked_div(duration)?;
         msg!("interval_amount {}", interval_amount);
 
-        // (time - self.start_at) / self.interval + 1, current and passed intervals are unlocked
         let nr_intervals = time
             .checked_sub(self.start_time)?
             .checked_div(self.interval)?
             .checked_add(1)?;
         msg!("nr_intervals {}", nr_intervals);
 
-        // nr_intervals * interval_amount - self.withdrawal
         nr_intervals
             .checked_mul(interval_amount)?
             .checked_sub(self.withdrawal)
