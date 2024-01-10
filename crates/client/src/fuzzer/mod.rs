830aabee886c106caaa0acd56c29e8ebebf9aeee
lukacan <andrej@DESKTOP-UME2160.localdomain>
2024-01-10 22:18:48 +0100

    🚨 fmt + clippy

--- crates/client/src/fuzzer/mod.rs
+++ crates/client/src/fuzzer/mod.rs
@@ -1,9 +1,9 @@
-pub mod fuzzer_generator;
+pub mod accounts_storage;
 pub mod data_builder;
+pub mod fuzzer_generator;
 #[cfg(feature = "fuzzing")]
 pub mod program_test_client_blocking;
 pub mod snapshot;
-pub mod accounts_storage;
 pub mod snapshot_generator;
 
 pub type AccountId = u8;
