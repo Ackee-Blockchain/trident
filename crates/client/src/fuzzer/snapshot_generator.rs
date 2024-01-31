65a7699b955051e374efd36a0db23c89cc68b794
Ikrk <ikrk@centrum.cz>
2024-01-31 14:14:23 +0100

    ✨ Parsing of additional anchor types

--- crates/client/src/fuzzer/snapshot_generator.rs
+++ crates/client/src/fuzzer/snapshot_generator.rs
@@ -1,12 +1,17 @@
+// To generate the snapshot data types, we need to first find all context struct within the program and parse theirs accounts.
+// The parsing was mostly taken over from Anchor implementation:
+// https://github.com/coral-xyz/anchor/blob/master/lang/syn/src/parser/accounts/mod.rs
+
 use std::{error::Error, fs::File, io::Read};
 
 use cargo_metadata::camino::Utf8PathBuf;
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
 use quote::{format_ident, quote, ToTokens};
 use syn::parse::{Error as ParseError, Result as ParseResult};
 use syn::spanned::Spanned;
 use syn::{
-    parse_quote, Attribute, Fields, GenericArgument, Item, ItemStruct, PathArguments, TypePath,
+    parse_quote, Attribute, Fields, GenericArgument, Item, ItemStruct, Path, PathArguments,
+    TypePath,
 };
 
 pub fn generate_snapshots_code(code_path: Vec<(String, Utf8PathBuf)>) -> Result<String, String> {
@@ -170,25 +175,41 @@ fn deserialize_ctx_struct(orig_struct: &ItemStruct) -> Result<TokenStream, Box<d
                         ))
                     }
                 };
-                let field_type = &field.ty;
+                // let field_type = &field.ty;
 
-                let path = match &field_type {
-                    syn::Type::Path(ty_path) => ty_path.path.clone(),
-                    _ => {
-                        return Err(ParseError::new(
-                            field_type.span(),
-                            "invalid account type given",
-                        ))
-                    }
-                };
-                let id = path.segments[0].clone();
+                // let path = match &field_type {
+                //     syn::Type::Path(ty_path) => ty_path.path.clone(),
+                //     _ => {
+                //         return Err(ParseError::new(
+                //             field_type.span(),
+                //             "invalid account type given",
+                //         ))
+                //     }
+                // };
+                // let id: syn::PathSegment = path.segments[0].clone();
                 // println!("field name: {}, type: {}", field_name, id.ident);
-                let ty = match id.ident.to_string().as_str() {
+                let (ident, _optional, path) = ident_string(field)?;
+                let ty = match ident.as_str() {
                     "AccountInfo" => AnchorType::AccountInfo,
                     "Signer" => AnchorType::Signer,
                     "Account" => AnchorType::Account(parse_account_ty(&path)?),
                     "Program" => AnchorType::Program(parse_program_ty(&path)?), // TODO
-                    _ => return Err(ParseError::new(id.span(), "invalid account type given")),
+                    "Sysvar" => AnchorType::Sysvar(parse_sysvar(&path)?),
+                    "UncheckedAccount" => AnchorType::UncheckedAccount,
+                    "AccountLoader" => {
+                        AnchorType::AccountLoader(parse_program_account_loader(&path)?)
+                    }
+                    "Interface" => AnchorType::Interface(parse_interface_ty(&path)?),
+                    "InterfaceAccount" => {
+                        AnchorType::InterfaceAccount(parse_interface_account_ty(&path)?)
+                    }
+                    "SystemAccount" => AnchorType::SystemAccount,
+                    _ => {
+                        return Err(ParseError::new(
+                            field.ty.span(),
+                            "invalid account type given",
+                        ))
+                    }
                 };
                 let deser_tokens = match ty.to_tokens() {
                     Some((return_type, deser_method)) => {
@@ -239,35 +260,34 @@ fn deserialize_ctx_struct(orig_struct: &ItemStruct) -> Result<TokenStream, Box<d
 // TODO add all account types as in https://github.com/coral-xyz/anchor/blob/master/lang/syn/src/parser/accounts/mod.rs#L351
 pub enum AnchorType {
     AccountInfo,
+    UncheckedAccount,
     Signer,
     Account(AccountTy),
+    AccountLoader(AccountLoaderTy),
     Program(ProgramTy),
+    SystemAccount,
+    Sysvar(SysvarTy),
+    Interface(InterfaceTy),
+    InterfaceAccount(InterfaceAccountTy),
 }
 
-impl AnchorType {
-    pub fn to_tokens(&self) -> Option<(TokenStream, TokenStream)> {
-        let (return_type, deser_method) = match self {
-            AnchorType::AccountInfo => return None,
-            AnchorType::Signer => (
-                quote! { Signer<'_>},
-                quote!(anchor_lang::accounts::signer::Signer::try_from(&acc)),
-            ),
-            AnchorType::Account(acc) => {
-                let path = &acc.account_type_path;
-                (
-                    quote! { anchor_lang::accounts::account::Account<#path>},
-                    quote! {anchor_lang::accounts::account::Account::try_from(&acc)},
-                )
-            }
-            AnchorType::Program(prog) => {
-                let path = &prog.account_type_path;
-                (
-                    quote! { anchor_lang::accounts::program::Program<#path>},
-                    quote!(anchor_lang::accounts::program::Program::try_from(&acc)),
-                )
-            }
-        };
-        Some((return_type, deser_method))
+#[derive(Debug, PartialEq, Eq)]
+pub enum SysvarTy {
+    Clock,
+    Rent,
+    EpochSchedule,
+    Fees,
+    RecentBlockhashes,
+    SlotHashes,
+    SlotHistory,
+    StakeHistory,
+    Instructions,
+    Rewards,
+}
+
+impl std::fmt::Display for SysvarTy {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
 
@@ -281,6 +301,85 @@ pub struct AccountTy {
     pub account_type_path: TypePath,
     // True if the account has been boxed via `Box<T>`.
     pub boxed: bool,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct AccountLoaderTy {
+    // The struct type of the account.
+    pub account_type_path: TypePath,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct InterfaceTy {
+    // The struct type of the account.
+    pub account_type_path: TypePath,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct InterfaceAccountTy {
+    // The struct type of the account.
+    pub account_type_path: TypePath,
+    // True if the account has been boxed via `Box<T>`.
+    pub boxed: bool,
+}
+
+impl AnchorType {
+    pub fn to_tokens(&self) -> Option<(TokenStream, TokenStream)> {
+        let (return_type, deser_method) = match self {
+            AnchorType::AccountInfo | AnchorType::UncheckedAccount => return None,
+            AnchorType::SystemAccount => (
+                quote! { SystemAccount<'_>},
+                quote!(anchor_lang::accounts::system_account::SystemAccount::try_from(&acc)),
+            ),
+            AnchorType::Sysvar(sysvar) => {
+                let id = syn::Ident::new(sysvar.to_string().as_str(), Span::call_site());
+                (
+                    quote! { SysVar<#id>},
+                    quote!(anchor_lang::accounts::sysvar::Sysvar::try_from(&acc)),
+                )
+            }
+            AnchorType::Signer => (
+                quote! { Signer<'_>},
+                quote!(anchor_lang::accounts::signer::Signer::try_from(&acc)),
+            ),
+            AnchorType::Account(acc) => {
+                let path = &acc.account_type_path;
+                (
+                    quote! { anchor_lang::accounts::account::Account<#path>},
+                    quote! {anchor_lang::accounts::account::Account::try_from(&acc)},
+                )
+            }
+            AnchorType::AccountLoader(acc) => {
+                let path = &acc.account_type_path;
+                (
+                    quote! { anchor_lang::accounts::account::Account<#path>},
+                    quote! {anchor_lang::accounts::account::Account::try_from(&acc)},
+                )
+            }
+            AnchorType::Program(prog) => {
+                let path = &prog.account_type_path;
+                (
+                    quote! { anchor_lang::accounts::program::Program<#path>},
+                    quote!(anchor_lang::accounts::program::Program::try_from(&acc)),
+                )
+            }
+            AnchorType::Interface(interf) => {
+                let path = &interf.account_type_path;
+                (
+                    quote! { anchor_lang::accounts::interface::Interface<#path>},
+                    quote! {anchor_lang::accounts::interface::Interface::try_from(&acc)},
+                )
+            }
+            AnchorType::InterfaceAccount(interf_acc) => {
+                let path = &interf_acc.account_type_path;
+                (
+                    quote! { anchor_lang::accounts::interface_account::InterfaceAccount<#path>},
+                    quote! {anchor_lang::accounts::interface_account::InterfaceAccount::try_from(&acc)},
+                )
+            }
+        };
+        Some((return_type, deser_method))
+    }
 }
 
 fn deserialize_tokens(
@@ -306,6 +405,7 @@ fn acc_info_tokens(name: &syn::Ident) -> TokenStream {
     }
 }
 
+// Copied from Anchor
 fn parse_account_ty(path: &syn::Path) -> ParseResult<AccountTy> {
     let account_type_path = parse_account(path)?;
     let boxed = tts_to_string(path)
@@ -317,17 +417,46 @@ fn parse_account_ty(path: &syn::Path) -> ParseResult<AccountTy> {
     })
 }
 
+// Copied from Anchor
 fn parse_program_ty(path: &syn::Path) -> ParseResult<ProgramTy> {
     let account_type_path = parse_account(path)?;
     Ok(ProgramTy { account_type_path })
 }
 
+// Copied from Anchor
+fn parse_program_account_loader(path: &syn::Path) -> ParseResult<AccountLoaderTy> {
+    let account_ident = parse_account(path)?;
+    Ok(AccountLoaderTy {
+        account_type_path: account_ident,
+    })
+}
+
+// Copied from Anchor
+fn parse_interface_ty(path: &syn::Path) -> ParseResult<InterfaceTy> {
+    let account_type_path = parse_account(path)?;
+    Ok(InterfaceTy { account_type_path })
+}
+
+// Copied from Anchor
+fn parse_interface_account_ty(path: &syn::Path) -> ParseResult<InterfaceAccountTy> {
+    let account_type_path = parse_account(path)?;
+    let boxed = tts_to_string(path)
+        .replace(' ', "")
+        .starts_with("Box<InterfaceAccount<");
+    Ok(InterfaceAccountTy {
+        account_type_path,
+        boxed,
+    })
+}
+
+// Copied from Anchor
 pub fn tts_to_string<T: quote::ToTokens>(item: T) -> String {
     let mut tts = proc_macro2::TokenStream::new();
     item.to_tokens(&mut tts);
     tts.to_string()
 }
 
+// Copied from Anchor
 fn parse_account(mut path: &syn::Path) -> ParseResult<syn::TypePath> {
     let path_str = tts_to_string(path).replace(' ', "");
     if path_str.starts_with("Box<Account<") || path_str.starts_with("Box<InterfaceAccount<") {
@@ -387,6 +516,66 @@ fn parse_account(mut path: &syn::Path) -> ParseResult<syn::TypePath> {
     }
 }
 
+// Copied from Anchor
+fn parse_sysvar(path: &syn::Path) -> ParseResult<SysvarTy> {
+    let segments = &path.segments[0];
+    let account_ident = match &segments.arguments {
+        syn::PathArguments::AngleBracketed(args) => {
+            // Expected: <'info, MyType>.
+            if args.args.len() != 2 {
+                return Err(ParseError::new(
+                    args.args.span(),
+                    "bracket arguments must be the lifetime and type",
+                ));
+            }
+            match &args.args[1] {
+                syn::GenericArgument::Type(syn::Type::Path(ty_path)) => {
+                    // TODO: allow segmented paths.
+                    if ty_path.path.segments.len() != 1 {
+                        return Err(ParseError::new(
+                            ty_path.path.span(),
+                            "segmented paths are not currently allowed",
+                        ));
+                    }
+                    let path_segment = &ty_path.path.segments[0];
+                    path_segment.ident.clone()
+                }
+                _ => {
+                    return Err(ParseError::new(
+                        args.args[1].span(),
+                        "first bracket argument must be a lifetime",
+                    ))
+                }
+            }
+        }
+        _ => {
+            return Err(ParseError::new(
+                segments.arguments.span(),
+                "expected angle brackets with a lifetime and type",
+            ))
+        }
+    };
+    let ty = match account_ident.to_string().as_str() {
+        "Clock" => SysvarTy::Clock,
+        "Rent" => SysvarTy::Rent,
+        "EpochSchedule" => SysvarTy::EpochSchedule,
+        "Fees" => SysvarTy::Fees,
+        "RecentBlockhashes" => SysvarTy::RecentBlockhashes,
+        "SlotHashes" => SysvarTy::SlotHashes,
+        "SlotHistory" => SysvarTy::SlotHistory,
+        "StakeHistory" => SysvarTy::StakeHistory,
+        "Instructions" => SysvarTy::Instructions,
+        "Rewards" => SysvarTy::Rewards,
+        _ => {
+            return Err(ParseError::new(
+                account_ident.span(),
+                "invalid sysvar provided",
+            ))
+        }
+    };
+    Ok(ty)
+}
+
 fn has_program_attribute(attrs: &Vec<Attribute>) -> bool {
     for attr in attrs {
         if attr.path.is_ident("program") {
@@ -394,4 +583,65 @@ fn has_program_attribute(attrs: &Vec<Attribute>) -> bool {
         }
     }
     false
+}
+
+// Copied from Anchor
+fn option_to_inner_path(path: &Path) -> ParseResult<Path> {
+    let segment_0 = path.segments[0].clone();
+    match segment_0.arguments {
+        syn::PathArguments::AngleBracketed(args) => {
+            if args.args.len() != 1 {
+                return Err(ParseError::new(
+                    args.args.span(),
+                    "can only have one argument in option",
+                ));
+            }
+            match &args.args[0] {
+                syn::GenericArgument::Type(syn::Type::Path(ty_path)) => Ok(ty_path.path.clone()),
+                _ => Err(ParseError::new(
+                    args.args[1].span(),
+                    "first bracket argument must be a lifetime",
+                )),
+            }
+        }
+        _ => Err(ParseError::new(
+            segment_0.arguments.span(),
+            "expected angle brackets with a lifetime and type",
+        )),
+    }
+}
+
+// Copied from Anchor
+fn ident_string(f: &syn::Field) -> ParseResult<(String, bool, Path)> {
+    let mut path = match &f.ty {
+        syn::Type::Path(ty_path) => ty_path.path.clone(),
+        _ => return Err(ParseError::new(f.ty.span(), "invalid account type given")),
+    };
+    let mut optional = false;
+    if tts_to_string(&path).replace(' ', "").starts_with("Option<") {
+        path = option_to_inner_path(&path)?;
+        optional = true;
+    }
+    if tts_to_string(&path)
+        .replace(' ', "")
+        .starts_with("Box<Account<")
+    {
+        return Ok(("Account".to_string(), optional, path));
+    }
+    if tts_to_string(&path)
+        .replace(' ', "")
+        .starts_with("Box<InterfaceAccount<")
+    {
+        return Ok(("InterfaceAccount".to_string(), optional, path));
+    }
+    // TODO: allow segmented paths.
+    if path.segments.len() != 1 {
+        return Err(ParseError::new(
+            f.ty.span(),
+            "segmented paths are not currently allowed",
+        ));
+    }
+
+    let segments = &path.segments[0];
+    Ok((segments.ident.to_string(), optional, path))
 }
