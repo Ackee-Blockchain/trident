9c5f6a5be0478072251e4bb5ee8e5cc890d64ac9
lukacan <andrej.lukacovic98@gmail.com>
2024-07-05 01:10:30 +0200

    🔥 Remove fuzzing flag

--- crates/fuzz/src/lib.rs
+++ crates/fuzz/src/lib.rs
@@ -2,7 +2,6 @@ pub mod accounts_storage;
 pub mod data_builder;
 pub mod error;
 pub mod fuzzing_stats;
-// #[cfg(feature = "fuzzing")]
 pub mod program_test_client_blocking;
 pub mod snapshot;
 pub type AccountId = u8;
