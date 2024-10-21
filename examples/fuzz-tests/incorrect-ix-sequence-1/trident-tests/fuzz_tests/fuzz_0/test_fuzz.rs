b2f7bb4975313d2368bc611630dc3bdf5a3ee85a
lukacan <andrej.lukacovic98@gmail.com>
2024-10-21 17:04:14 +0200

    🔒️ Update examples

--- examples/fuzz-tests/incorrect-ix-sequence-1/trident-tests/fuzz_tests/fuzz_0/test_fuzz.rs
+++ examples/fuzz-tests/incorrect-ix-sequence-1/trident-tests/fuzz_tests/fuzz_0/test_fuzz.rs
@@ -1,23 +1,30 @@
 use fuzz_instructions::Initialize;
 use trident_client::fuzzing::*;
-
 mod fuzz_instructions;
-
 use fuzz_instructions::FuzzInstruction;
 use incorrect_ix_sequence_1::entry as entry_incorrect_ix_sequence_1;
 use incorrect_ix_sequence_1::ID as PROGRAM_ID_INCORRECT_IX_SEQUENCE_1;
-
 const PROGRAM_NAME_INCORRECT_IX_SEQUENCE_1: &str = "incorrect_ix_sequence_1";
-
 struct MyFuzzData;
-
+/// Define instruction sequences for invocation.
+/// `pre_ixs` runs at the start, `ixs` in the middle, and `post_ixs` at the end.
+/// For example, to call `InitializeFn` at the start of each fuzzing iteration:
+/// ```
+/// fn pre_ixs(u: &mut arbitrary::Unstructured) ->
+/// arbitrary::Result<Vec<FuzzInstruction>> {
+///     let init = FuzzInstruction::InitializeFn(InitializeFn::arbitrary(u)?);
+///     Ok(vec![init])
+/// }
+/// ```
+/// For more details, see: https://ackee.xyz/trident/docs/dev/features/instructions-sequences/#instructions-sequences
 impl FuzzDataBuilder<FuzzInstruction> for MyFuzzData {
     fn pre_ixs(u: &mut arbitrary::Unstructured) -> arbitrary::Result<Vec<FuzzInstruction>> {
         let init_ix = FuzzInstruction::Initialize(Initialize::arbitrary(u)?);
         Ok(vec![init_ix])
     }
 }
-
+/// `fn fuzz_iteration` runs during every fuzzing iteration.
+/// Modification is not required.
 fn fuzz_iteration<T: FuzzTestExecutor<U> + std::fmt::Display, U>(
     fuzz_data: FuzzData<T, U>,
     config: &Config,
@@ -27,15 +34,11 @@ fn fuzz_iteration<T: FuzzTestExecutor<U> + std::fmt::Display, U>(
         &PROGRAM_ID_INCORRECT_IX_SEQUENCE_1,
         processor!(convert_entry!(entry_incorrect_ix_sequence_1)),
     );
-
     let mut client =
         ProgramTestClientBlocking::new(&[fuzzing_program_incorrect_ix_sequence_1], config).unwrap();
-
     let _ = fuzz_data.run_with_runtime(&mut client, config);
 }
-
 fn main() {
     let config = Config::new();
-
     fuzz_trident ! (fuzz_ix : FuzzInstruction , | fuzz_data : MyFuzzData | { fuzz_iteration (fuzz_data , & config) ; });
 }
--- examples/fuzz-tests/unauthorized-access-2/trident-tests/fuzz_tests/fuzz_0/test_fuzz.rs
+++ examples/fuzz-tests/unauthorized-access-2/trident-tests/fuzz_tests/fuzz_0/test_fuzz.rs
@@ -1,16 +1,22 @@
 use fuzz_instructions::Initialize;
 use trident_client::fuzzing::*;
-
 mod fuzz_instructions;
-
 use fuzz_instructions::FuzzInstruction;
 use unauthorized_access_2::entry as entry_unauthorized_access_2;
 use unauthorized_access_2::ID as PROGRAM_ID_UNAUTHORIZED_ACCESS_2;
-
 const PROGRAM_NAME_UNAUTHORIZED_ACCESS_2: &str = "unauthorized_access_2";
-
 struct MyFuzzData;
-
+/// Define instruction sequences for invocation.
+/// `pre_ixs` runs at the start, `ixs` in the middle, and `post_ixs` at the end.
+/// For example, to call `InitializeFn` at the start of each fuzzing iteration:
+/// ```
+/// fn pre_ixs(u: &mut arbitrary::Unstructured) ->
+/// arbitrary::Result<Vec<FuzzInstruction>> {
+///     let init = FuzzInstruction::InitializeFn(InitializeFn::arbitrary(u)?);
+///     Ok(vec![init])
+/// }
+/// ```
+/// For more details, see: https://ackee.xyz/trident/docs/dev/features/instructions-sequences/#instructions-sequences
 impl FuzzDataBuilder<FuzzInstruction> for MyFuzzData {
     fn pre_ixs(u: &mut arbitrary::Unstructured) -> arbitrary::Result<Vec<FuzzInstruction>> {
         let init_ix = FuzzInstruction::Initialize(Initialize::arbitrary(u)?);
@@ -18,7 +24,8 @@ impl FuzzDataBuilder<FuzzInstruction> for MyFuzzData {
         Ok(vec![init_ix])
     }
 }
-
+/// `fn fuzz_iteration` runs during every fuzzing iteration.
+/// Modification is not required.
 fn fuzz_iteration<T: FuzzTestExecutor<U> + std::fmt::Display, U>(
     fuzz_data: FuzzData<T, U>,
     config: &Config,
@@ -28,15 +35,11 @@ fn fuzz_iteration<T: FuzzTestExecutor<U> + std::fmt::Display, U>(
         &PROGRAM_ID_UNAUTHORIZED_ACCESS_2,
         processor!(convert_entry!(entry_unauthorized_access_2)),
     );
-
     let mut client =
         ProgramTestClientBlocking::new(&[fuzzing_program_unauthorized_access_2], config).unwrap();
-
     let _ = fuzz_data.run_with_runtime(&mut client, config);
 }
-
 fn main() {
     let config = Config::new();
-
     fuzz_trident ! (fuzz_ix : FuzzInstruction , | fuzz_data : MyFuzzData | { fuzz_iteration (fuzz_data , & config) ; });
 }
