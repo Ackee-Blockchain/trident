375b99f91a096e97723955ff7cf776f6539276ac
lukacan <andrej.lukacovic98@gmail.com>
2025-01-06 11:50:48 +0100

    ⚡️ Update Fuzz Client implementation for TridentSVM

--- crates/fuzz/src/fuzz_client_impl.rs
+++ crates/fuzz/src/fuzz_client_impl.rs
@@ -65,15 +65,15 @@ impl FuzzClient for TridentSVM<'_> {
     }
 
     fn set_account_custom(&mut self, address: &Pubkey, account: &AccountSharedData) {
-        self.accounts.add_account(address, account);
+        self.add_temp_account(address, account);
     }
 
     fn payer(&self) -> solana_sdk::signature::Keypair {
-        self.payer.insecure_clone()
+        self.get_payer()
     }
 
     fn get_account(&mut self, key: &Pubkey) -> AccountSharedData {
-        self.accounts.get_account(key).unwrap_or_default()
+        trident_svm::trident_svm::TridentSVM::get_account(self, key).unwrap_or_default()
     }
 
     fn get_last_blockhash(&self) -> Hash {
@@ -84,6 +84,11 @@ impl FuzzClient for TridentSVM<'_> {
         &mut self,
         instructions: &[Instruction],
     ) -> Result<(), crate::error::FuzzClientError> {
+        // Currently Trident supports only one instruction in a transaction
+        if instructions.len() != 1 {
+            return Err(crate::error::FuzzClientError::Custom(55000));
+        }
+
         // there should be at least 1 RW fee-payer account.
         // But we do not pay for TX currently so has to be manually updated
         // tx.message.header.num_required_signatures = 1;
@@ -95,25 +100,29 @@ impl FuzzClient for TridentSVM<'_> {
 
         let result = self.process_transaction(tx);
 
-        // eprintln!("result: {:#?}", result.execution_results);
-        // eprintln!("result: {:#?}", result.loaded_transactions);
-
-        self.settle(&result);
+        // eprintln!("result1: {:#?}", result.execution_results);
+        // eprintln!("result2: {:#?}", result.loaded_transactions);
 
         match &result.execution_results[0] {
             solana_svm::transaction_results::TransactionExecutionResult::Executed {
                 details,
                 programs_modified_by_tx: _,
             } => match &details.status {
-                Ok(_) => Ok(()),
+                Ok(_) => match &result.loaded_transactions[0] {
+                    Ok(loaded_transaction) => {
+                        self.settle_accounts(&loaded_transaction.accounts);
+                        Ok(())
+                    }
+                    Err(_) => Err(crate::error::FuzzClientError::Custom(55001)),
+                },
                 Err(e) => match e {
                     TransactionError::InstructionError(_index, ix_error) => match ix_error {
                         solana_sdk::instruction::InstructionError::ProgramFailedToComplete => {
                             panic!("Program Failed to complete")
                         }
-                        _ => Err(crate::error::FuzzClientError::Custom(3)),
+                        _ => Err(crate::error::FuzzClientError::Custom(55002)),
                     },
-                    _ => Err(crate::error::FuzzClientError::Custom(4)),
+                    _ => Err(crate::error::FuzzClientError::Custom(55003)),
                 },
             },
             solana_svm::transaction_results::TransactionExecutionResult::NotExecuted(
@@ -123,15 +132,15 @@ impl FuzzClient for TridentSVM<'_> {
                     solana_sdk::instruction::InstructionError::ProgramFailedToComplete => {
                         panic!("Program Failed to complete")
                     }
-                    _ => Err(crate::error::FuzzClientError::Custom(3)),
+                    _ => Err(crate::error::FuzzClientError::Custom(55004)),
                 },
-                _ => Err(crate::error::FuzzClientError::Custom(4)),
+                _ => Err(crate::error::FuzzClientError::Custom(55005)),
             },
         }
     }
 
     fn get_sysvar<T: Sysvar>(&self) -> T {
-        self.get_sysvar::<T>()
+        trident_svm::trident_svm::TridentSVM::get_sysvar::<T>(self)
     }
 
     fn clear_accounts(&mut self) {
