fc9100b57685b7126b60571cfdc1dcf3066c496d
lukacan <andrej.lukacovic98@gmail.com>
2025-01-13 13:30:47 +0100

    🔥 Remove unnecessary function in the test_fuzz.rs

--- crates/client/tests/expected_source_codes/expected_test_fuzz.rs
+++ crates/client/tests/expected_source_codes/expected_test_fuzz.rs
@@ -17,15 +17,6 @@ struct InstructionsSequence;
 /// ```
 /// For more details, see: https://ackee.xyz/trident/docs/latest/features/instructions-sequences/#instructions-sequences
 impl FuzzDataBuilder<FuzzInstruction> for InstructionsSequence {}
-/// `fn fuzz_iteration` runs during every fuzzing iteration.
-/// Modification is not required.
-fn fuzz_iteration<T: FuzzTestExecutor<U> + std::fmt::Display, U>(
-    fuzz_data: FuzzData<T, U>,
-    config: &Config,
-    client: &mut impl FuzzClient,
-) {
-    let _ = fuzz_data.run_with_runtime(client, config);
-}
 fn main() {
     let program_additional_program = ProgramEntrypoint::new(
         pubkey!("fill corresponding program ID here"),
@@ -40,5 +31,5 @@ fn main() {
     let config = Config::new();
     let mut client =
         TridentSVM::new_client(&[program_additional_program, program_idl_test], &config);
-    fuzz_trident ! (fuzz_ix : FuzzInstruction , | fuzz_data : InstructionsSequence | { fuzz_iteration (fuzz_data , & config , & mut client) ; });
+    fuzz_trident ! (fuzz_ix : FuzzInstruction , | fuzz_data : InstructionsSequence , client : TridentSVM , config : Config |);
 }
--- crates/template/src/test_fuzz_generator.rs
+++ crates/template/src/test_fuzz_generator.rs
@@ -42,22 +42,16 @@ pub fn generate_source_code(_idl_instructions: &[Idl], lib_names: &[String]) -> 
         /// For more details, see: https://ackee.xyz/trident/docs/latest/features/instructions-sequences/#instructions-sequences
         impl FuzzDataBuilder<FuzzInstruction> for InstructionsSequence {}
 
-        /// `fn fuzz_iteration` runs during every fuzzing iteration.
-        /// Modification is not required.
-        fn fuzz_iteration<T: FuzzTestExecutor<U> + std::fmt::Display, U>(
-            fuzz_data: FuzzData<T, U>,
-            config: &Config,
-            client: &mut impl FuzzClient,
-        ) {
-            let _ = fuzz_data.run_with_runtime(client, config);
-        }
         fn main() {
 
             #(#programs)*
 
             let config = Config::new();
             let mut client = TridentSVM::new_client(&[ #(#input_array),* ], &config);
-            fuzz_trident ! (fuzz_ix : FuzzInstruction , | fuzz_data : InstructionsSequence | { fuzz_iteration (fuzz_data , & config,&mut client) ; });
+            fuzz_trident!(
+                fuzz_ix: FuzzInstruction,
+                |fuzz_data: InstructionsSequence, client: TridentSVM, config: Config|
+            );
         }
     };
 
