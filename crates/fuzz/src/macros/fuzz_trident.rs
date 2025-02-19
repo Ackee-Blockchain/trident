7ab059d93cfa8a199b09fe2dc594b25053c0f83e
hyckomatej <hycko.matej@gmail.com>
2025-02-19 08:45:06 +0100

    Update honggfuzz debug

--- crates/fuzz/src/macros/fuzz_trident.rs
+++ crates/fuzz/src/macros/fuzz_trident.rs
@@ -38,6 +38,26 @@ macro_rules! fuzz_trident {
                 // TODO: the function might not need to return anything
                 let _ = $buf.run_with_runtime(&mut $client, &$config);
             });
+        } else if cfg!(honggfuzz_debug) {
+            let mut crash_file = String::new();
+            std::io::stdin()
+                .read_line(&mut crash_file)
+                .expect("Failed to read crash file path from stdin");
+            let crash_file = crash_file.trim();
+
+            let mut $buf = {
+                let fuzz_data = std::fs::read(crash_file).expect("Failed to read crash file");
+                use arbitrary::Unstructured;
+
+                let mut buf = Unstructured::new(&fuzz_data);
+                if let Ok(fuzz_data) = build_ix_fuzz_data($dty {}, &mut buf) {
+                    fuzz_data
+                } else {
+                    return;
+                }
+            };
+            // TODO: the function might not need to return anything
+            let _ = $buf.run_with_runtime(&mut $client, &$config);
         } else {
             panic!("Select Honggfuzz or AFL for fuzzing!!!")
         }
