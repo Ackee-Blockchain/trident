fd9ae2369497483e32fe03eb3fbfaab0214c53b8
lukacan <andrej.lukacovic98@gmail.com>
2025-03-02 11:04:59 +0100

    ✅ Update pipeline examples

--- examples/cpi/simple-cpi-6/trident-tests/fuzz_0/instructions/initialize_caller.rs
+++ examples/cpi/simple-cpi-6/trident-tests/fuzz_0/instructions/initialize_caller.rs
@@ -3,7 +3,7 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use trident_fuzz::fuzzing::*;
 #[derive(Arbitrary, TridentInstruction)]
 #[program_id("FWtSodrkUnovFPnNRCxneP6VWh6JH6jtQZ4PHoP8Ejuz")]
-#[discriminator([204u8, 76u8, 200u8, 172u8, 185u8, 14u8, 99u8, 166u8])]
+# [discriminator ([204u8 , 76u8 , 200u8 , 172u8 , 185u8 , 14u8 , 99u8 , 166u8 ,])]
 pub struct InitializeCallerInstruction {
     pub accounts: InitializeCallerInstructionAccounts,
     pub data: InitializeCallerInstructionData,
@@ -11,6 +11,7 @@ pub struct InitializeCallerInstruction {
 /// Instruction Accounts
 #[derive(Arbitrary, Debug, Clone, TridentAccounts)]
 #[instruction_data(InitializeCallerInstructionData)]
+#[storage(FuzzAccounts)]
 pub struct InitializeCallerInstructionAccounts {
     #[account(signer, mut,storage = signer)]
     pub signer: TridentAccount,
@@ -20,7 +21,7 @@ pub struct InitializeCallerInstructionAccounts {
 /// Instruction Data
 #[derive(Arbitrary, Debug, BorshDeserialize, BorshSerialize, Clone)]
 pub struct InitializeCallerInstructionData {
-    pub input: u16,
+    input: u16,
 }
 /// Implementation of instruction setters for fuzzing
 ///
@@ -28,12 +29,8 @@ pub struct InitializeCallerInstructionData {
 /// - Set instruction data during fuzzing
 /// - Configure instruction accounts during fuzzing
 /// - (Optional) Set remaining accounts during fuzzing
+///
+/// Docs: https://ackee.xyz/trident/docs/latest/start-fuzzing/writting-fuzz-test/
 impl InstructionSetters for InitializeCallerInstruction {
     type IxAccounts = FuzzAccounts;
-    fn set_accounts(
-        &mut self,
-        _client: &mut impl FuzzClient,
-        _fuzz_accounts: &mut Self::IxAccounts,
-    ) {
-    }
 }
--- examples/cpi/simple-cpi-6/trident-tests/fuzz_0/test_fuzz.rs
+++ examples/cpi/simple-cpi-6/trident-tests/fuzz_0/test_fuzz.rs
@@ -1,4 +1,4 @@
-use fuzz_transactions::FuzzTransactions;
+use fuzz_transactions::*;
 use trident_fuzz::fuzzing::*;
 mod fuzz_transactions;
 mod instructions;
@@ -7,51 +7,38 @@ mod types;
 use callee::entry as entry_callee;
 use caller::entry as entry_caller;
 pub use transactions::*;
-struct TransactionsSequence;
-/// Define transaction sequences for execution.
-/// `starting_sequence` runs at the start, `middle` in the middle, and `ending`
-/// at the end.
-/// For example, to call `InitializeFn`, `UpdateFn` and then `WithdrawFn` during
-/// each fuzzing iteration:
-/// ```
-/// impl FuzzDataBuilder<FuzzTransactions> for InstructionsSequence {
-///     fn starting_sequence(fuzzer_data: &mut FuzzerData) ->
-/// SequenceResult<FuzzTransactions> {
-///         let seq1 = sequence!([InitializeFn, UpdateFn], fuzzer_data);
-///         Ok(seq1)
-///     }
-///     fn middle_sequence(fuzzer_data: &mut FuzzerData) ->
-/// SequenceResult<FuzzTransactions> {
-///         let seq1 = sequence!([WithdrawFn], fuzzer_data);
-///         Ok(seq1)
-///     }
-///}
-/// ```
-/// For more details, see: https://ackee.xyz/trident/docs/latest/features/instructions-sequences/#instructions-sequences
-impl FuzzSequenceBuilder<FuzzTransactions> for TransactionsSequence {
-    fn starting_sequence(fuzzer_data: &mut FuzzerData) -> SequenceResult<FuzzTransactions> {
-        let seq1 = sequence!([InitializeCallerTransaction], fuzzer_data);
-        Ok(seq1)
+#[derive(Default, FuzzTestExecutor)]
+struct FuzzTest {
+    client: TridentSVM,
+}
+#[flow_executor]
+impl FuzzTest {
+    #[init]
+    fn start(&mut self) {
+        self.client.deploy_native_program(ProgramEntrypoint::new(
+            pubkey!("HJR1TK8bgrUWzysdpS1pBGBYKF7zi1tU9cS4qj8BW8ZL"),
+            None,
+            processor!(entry_callee),
+        ));
+        self.client.deploy_native_program(ProgramEntrypoint::new(
+            pubkey!("FWtSodrkUnovFPnNRCxneP6VWh6JH6jtQZ4PHoP8Ejuz"),
+            None,
+            processor!(entry_caller),
+        ));
     }
-    fn middle_sequence(_fuzzer_data: &mut FuzzerData) -> SequenceResult<FuzzTransactions> {
-        Ok(vec![])
-    }
-    fn ending_sequence(_fuzzer_data: &mut FuzzerData) -> SequenceResult<FuzzTransactions> {
-        Ok(vec![])
+    #[flow]
+    fn flow1(
+        &mut self,
+        fuzzer_data: &mut FuzzerData,
+        accounts: &mut FuzzAccounts,
+    ) -> Result<(), FuzzingError> {
+        InitializeCallerTransaction::build(fuzzer_data, &mut self.client, accounts)?
+            .execute(&mut self.client)?;
+
+        Ok(())
     }
 }
 fn main() {
-    let program_callee = ProgramEntrypoint::new(
-        pubkey!("HJR1TK8bgrUWzysdpS1pBGBYKF7zi1tU9cS4qj8BW8ZL"),
-        None,
-        processor!(entry_callee),
-    );
-    let program_caller = ProgramEntrypoint::new(
-        pubkey!("FWtSodrkUnovFPnNRCxneP6VWh6JH6jtQZ4PHoP8Ejuz"),
-        None,
-        processor!(entry_caller),
-    );
-    let config = TridentConfig::new();
-    let mut client = TridentSVM::new_client(&[program_callee, program_caller], &config);
-    fuzz_trident!(| fuzz_data : TransactionsSequence , client : TridentSVM , config : TridentConfig |);
+    let client = TridentSVM::new_client(&[], &TridentConfig::new());
+    FuzzTest::new(client).fuzz();
 }
