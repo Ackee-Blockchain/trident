e7000e116791b003d08ae38958d5de66ff0a39f1
lukacan <andrej.lukacovic98@gmail.com>
2025-03-03 16:15:54 +0100

    🚀 Add support for ranodm tail attribute

--- examples/trident-benchmark/maze_0/trident-tests/fuzz_0/fuzz_transactions.rs
+++ examples/trident-benchmark/maze_0/trident-tests/fuzz_0/fuzz_transactions.rs
@@ -1,15 +1,11 @@
-#![allow(clippy::large_enum_variant)]
-#![allow(clippy::enum_variant_names)]
 use crate::transactions::*;
 use trident_fuzz::fuzzing::*;
 /// FuzzTransactions contains all available transactions
 ///
-/// Below, the transaction variants are defined.
-/// Each variant contains a transaction struct.
-/// The transaction struct contains the instruction and the accounts and data.
-///
 /// You can create your own transactions by adding new variants to the enum.
-#[derive(Arbitrary, FuzzTestExecutor)]
+///
+/// Docs: https://ackee.xyz/trident/docs/latest/trident-api-macro/trident-types/fuzz-transactions/
+#[derive(Arbitrary, TransactionSelector)]
 pub enum FuzzTransactions {
     InitializeTransaction(InitializeTransaction),
     MoveEastTransaction(MoveEastTransaction),
@@ -17,10 +13,13 @@ pub enum FuzzTransactions {
     MoveSouthTransaction(MoveSouthTransaction),
     MoveWestTransaction(MoveWestTransaction),
 }
-/// Check supported AccountsStorages at
-/// https://ackee.xyz/trident/docs/latest/features/account-storages/
+/// FuzzAccounts contains all available accounts
+///
+/// You can create your own accounts by adding new fields to the struct.
+///
+/// Docs: https://ackee.xyz/trident/docs/latest/trident-api-macro/trident-types/fuzz-accounts/
 #[derive(Default)]
 pub struct FuzzAccounts {
-    pub state: AccountsStorage<PdaStore>,
-    pub state_author: AccountsStorage<KeypairStore>,
+    pub state_author: AccountsStorage,
+    pub state: AccountsStorage,
 }
