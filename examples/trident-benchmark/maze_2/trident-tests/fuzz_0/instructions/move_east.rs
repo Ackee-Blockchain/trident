1c63ce679458e9576efde9893faa8cd390f51bff
lukacan <andrej.lukacovic98@gmail.com>
2025-03-03 22:05:09 +0100

    ✨ Add method to TransactionSelector to allow random selection and execution of transactions

--- examples/trident-benchmark/maze_2/trident-tests/fuzz_0/instructions/move_east.rs
+++ examples/trident-benchmark/maze_2/trident-tests/fuzz_0/instructions/move_east.rs
@@ -1,8 +1,7 @@
 use crate::fuzz_transactions::FuzzAccounts;
 use borsh::{BorshDeserialize, BorshSerialize};
 use trident_fuzz::fuzzing::*;
-#[derive(Arbitrary, Debug, TridentInstruction)]
-#[accounts("accounts")]
+#[derive(Arbitrary, TridentInstruction)]
 #[program_id("5e554BrmQN7a2nbKrSUUxP8PMbq55rMntnkoCPmwr3Aq")]
 # [discriminator ([220u8 , 96u8 , 254u8 , 139u8 , 6u8 , 133u8 , 127u8 , 93u8 ,])]
 pub struct MoveEastInstruction {
@@ -11,20 +10,23 @@ pub struct MoveEastInstruction {
 }
 /// Instruction Accounts
 #[derive(Arbitrary, Debug, Clone, TridentAccounts)]
+#[instruction_data(MoveEastInstructionData)]
+#[storage(FuzzAccounts)]
 pub struct MoveEastInstructionAccounts {
-    pub state: TridentAccount,
+    #[account(mut,storage = state)]
+    state: TridentAccount,
 }
 /// Instruction Data
 #[derive(Arbitrary, Debug, BorshDeserialize, BorshSerialize, Clone)]
 pub struct MoveEastInstructionData {
-    pub p0: u64,
-    pub p1: u64,
-    pub p2: u64,
-    pub p3: u64,
-    pub p4: u64,
-    pub p5: u64,
-    pub p6: u64,
-    pub p7: u64,
+    p0: u64,
+    p1: u64,
+    p2: u64,
+    p3: u64,
+    p4: u64,
+    p5: u64,
+    p6: u64,
+    p7: u64,
 }
 /// Implementation of instruction setters for fuzzing
 ///
@@ -32,14 +34,8 @@ pub struct MoveEastInstructionData {
 /// - Set instruction data during fuzzing
 /// - Configure instruction accounts during fuzzing
 /// - (Optional) Set remaining accounts during fuzzing
+///
+/// Docs: https://ackee.xyz/trident/docs/latest/start-fuzzing/writting-fuzz-test/
 impl InstructionHooks for MoveEastInstruction {
     type IxAccounts = FuzzAccounts;
-    fn set_accounts(
-        &mut self,
-        _client: &mut impl FuzzClient,
-        fuzz_accounts: &mut Self::IxAccounts,
-    ) {
-        let state = fuzz_accounts.state.get(1);
-        self.accounts.state.set_account_meta(state, false, true);
-    }
 }
