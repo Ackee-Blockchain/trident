13949ae350348fa178f1fb90e72ed9e11d283516
andrej <andrej@andrejs-MacBook-Pro.local>
2025-08-11 11:31:21 +0200

    🚑️ Update gh pipelines

--- examples/trident-benchmark/maze_2/trident-tests/fuzz_0/instructions/move_west.rs
+++ examples/trident-benchmark/maze_2/trident-tests/fuzz_0/instructions/move_west.rs
@@ -1,13 +1,17 @@
-use crate::fuzz_transactions::FuzzAccounts;
-use borsh::{BorshDeserialize, BorshSerialize};
+use crate::fuzz_accounts::FuzzAccounts;
+use crate::types::*;
+use borsh::BorshDeserialize;
+use borsh::BorshSerialize;
 use trident_fuzz::fuzzing::*;
+
 #[derive(TridentInstruction, Default)]
 #[program_id("5e554BrmQN7a2nbKrSUUxP8PMbq55rMntnkoCPmwr3Aq")]
-# [discriminator ([122u8 , 187u8 , 56u8 , 38u8 , 248u8 , 122u8 , 182u8 , 106u8 ,])]
+#[discriminator([122u8, 187u8, 56u8, 38u8, 248u8, 122u8, 182u8, 106u8])]
 pub struct MoveWestInstruction {
     pub accounts: MoveWestInstructionAccounts,
     pub data: MoveWestInstructionData,
 }
+
 /// Instruction Accounts
 #[derive(Debug, Clone, TridentAccounts, Default)]
 #[instruction_data(MoveWestInstructionData)]
@@ -20,18 +24,27 @@ pub struct MoveWestInstructionAccounts {
     )]
     pub state: TridentAccount,
 }
+
 /// Instruction Data
 #[derive(Debug, BorshDeserialize, BorshSerialize, Clone, Default)]
 pub struct MoveWestInstructionData {
     pub p0: u64,
+
     pub p1: u64,
+
     pub p2: u64,
+
     pub p3: u64,
+
     pub p4: u64,
+
     pub p5: u64,
+
     pub p6: u64,
+
     pub p7: u64,
 }
+
 /// Implementation of instruction setters for fuzzing
 ///
 /// Provides methods to:
@@ -42,19 +55,15 @@ pub struct MoveWestInstructionData {
 /// Docs: https://ackee.xyz/trident/docs/latest/start-fuzzing/writting-fuzz-test/
 impl InstructionHooks for MoveWestInstruction {
     type IxAccounts = FuzzAccounts;
-    fn set_data(
-        &mut self,
-        _client: &mut impl FuzzClient,
-        _fuzz_accounts: &mut Self::IxAccounts,
-        rng: &mut TridentRng,
-    ) {
-        self.data.p0 = rng.gen_range(0..u64::MAX);
-        self.data.p1 = rng.gen_range(0..u64::MAX);
-        self.data.p2 = rng.gen_range(0..u64::MAX);
-        self.data.p3 = rng.gen_range(0..u64::MAX);
-        self.data.p4 = rng.gen_range(0..u64::MAX);
-        self.data.p5 = rng.gen_range(0..u64::MAX);
-        self.data.p6 = rng.gen_range(0..u64::MAX);
-        self.data.p7 = rng.gen_range(0..u64::MAX);
+
+    fn set_data(&mut self, trident: &mut Trident, _fuzz_accounts: &mut Self::IxAccounts) {
+        self.data.p0 = trident.gen_range(0..u64::MAX);
+        self.data.p1 = trident.gen_range(0..u64::MAX);
+        self.data.p2 = trident.gen_range(0..u64::MAX);
+        self.data.p3 = trident.gen_range(0..u64::MAX);
+        self.data.p4 = trident.gen_range(0..u64::MAX);
+        self.data.p5 = trident.gen_range(0..u64::MAX);
+        self.data.p6 = trident.gen_range(0..u64::MAX);
+        self.data.p7 = trident.gen_range(0..u64::MAX);
     }
 }
