4482f6600b5d224b4137b65c8099c4de7ef19071
hyckomat <hycko.matej@gmail.com>
2025-08-12 10:15:42 +0200

    Update coverage config and remove rustflags

--- crates/client/src/coverage/constants.rs
+++ crates/client/src/coverage/constants.rs
@@ -5,7 +5,7 @@ pub const PROFRAW_FILENAME: &str = "fuzz-cov-build-%p-%30m.profraw";
 pub const JSON_REPORT_FILENAME: &str = "coverage-report.json";
 pub const HTML_REPORT_DIRNAME: &str = "coverage-report";
 pub const COVERAGE_RUSTFLAGS: &str =
-    "-C instrument-coverage -C link-arg=-fuse-ld=lld -C link-arg=-lprofiler";
+    "-C instrument-coverage";
 
 // COVERAGE SERVER
 pub const SETUP_DYNAMIC_COVERAGE: &str = "/setup-dynamic-coverage";
