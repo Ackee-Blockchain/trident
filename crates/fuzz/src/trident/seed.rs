c69272b29554bafaa0deb4317be6d8a70e6d9804
lukacan <andrej.lukacovic@ackee.xyz>
2025-10-22 14:11:48 +0200

    ✨ Move some logic of flow execution from macro into fuzz crate for better readability

--- crates/fuzz/src/trident/seed.rs
+++ crates/fuzz/src/trident/seed.rs
@@ -5,26 +5,22 @@ use trident_fuzz_metrics::types::Seed;
 use crate::trident::Trident;
 
 impl Trident {
-    #[doc(hidden)]
-    pub fn _set_master_seed_for_debug(&mut self, seed: Seed) {
+    pub(crate) fn set_master_seed_for_debug(&mut self, seed: Seed) {
         self.rng.set_master_seed_for_debug(seed);
         self.fuzzing_data.add_master_seed(&hex::encode(seed));
     }
 
-    #[doc(hidden)]
-    pub fn _set_master_seed_and_thread_id(&mut self, seed: Seed, thread_id: usize) {
+    pub(crate) fn set_master_seed_and_thread_id(&mut self, seed: Seed, thread_id: usize) {
         self.rng.set_master_seed_and_thread_id(seed, thread_id);
         self.fuzzing_data.add_master_seed(&hex::encode(seed));
     }
 
-    #[doc(hidden)]
-    pub fn _next_iteration(&mut self) {
+    pub(crate) fn next_iteration(&mut self) {
         self.client.clear_accounts();
         self.rng.rotate_seed();
     }
 
-    #[doc(hidden)]
-    pub fn _get_fuzzing_data(&self) -> TridentFuzzingData {
+    pub(crate) fn get_fuzzing_data(&self) -> TridentFuzzingData {
         self.fuzzing_data.clone()
     }
 }
